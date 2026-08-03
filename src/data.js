1482dd7cd55ceb01b22166f655193528e1383a9a
Sachin-840480 <sachin1712003@gmail.com>
2026-08-03 13:11:43 +0530

    Added data

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const employees = [
+  { emp_no: 10001 },
+  { emp_no: 10002 },
+  { emp_no: 10003 }
+];
+
+export const salaries = [
+  {
+    emp_no: 10001,
+    salary: 50000,
+    from_date: "1990-01-01"
+  },
+  {
+    emp_no: 10001,
+    salary: 55000,
+    from_date: "1991-01-01"
+  },
+  {
+    emp_no: 10002,
+    salary: 45000,
+    from_date: "1992-01-01"
+  },
+  {
+    emp_no: 10003,
+    salary: 47000,
+    from_date: "1993-01-01"
+  }
+];
+
+export const titles = [
+  {
+    emp_no: 10001,
+    title: "Engineer",
+    from_date: "1990-01-01"
+  },
+  {
+    emp_no: 10002,
+    title: "Staff",
+    from_date: "1992-01-03"
+  }
+];
